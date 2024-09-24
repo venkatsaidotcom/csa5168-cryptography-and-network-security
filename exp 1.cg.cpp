4e728a60306ea7b6253b34d5ec3b1914615f069a
venkatsaidotcom <128777109+venkatsaidotcom@users.noreply.github.com>
2024-09-24 08:55:55 +0530

    Update exp 1.cg.cpp

--- exp 1.cg.cpp
+++ exp 1.cg.cpp
@@ -1,29 +1,26 @@
-#include <stdio.h>
-#include <ctype.h>
-void caesarCipher(char *text, int shift) {
-    char ch;
-    for (int i = 0; text[i] != '\0'; ++i) {
-        ch = text[i];
-        if (isalpha(ch)) {
-            if (isupper(ch)) {
-                text[i] = (ch + shift - 'A') % 26 + 'A';
-            }
-            else if (islower(ch)) {
-                text[i] = (ch + shift - 'a') % 26 + 'a';
-            }
-        }
-    }
+#include<stdio.h>
+#include<ctype.h>
+void CC(char *text,int k){
+	char ch;
+	int i;
+	for(i=0;text[i]!=0;i++){
+		ch=text[i];
+		if(isupper(ch)){
+			ch=((ch-'A'+k)%26)+'A';
+		}else if(islower(ch)){
+			ch=((ch-'a'+k)%26)+'a';
+		}
+		text[i]=ch;
+	}
 }
-int main() {
-    char text[100];
-    int shift;
-    printf("Enter a string: ");
-    fgets(text, sizeof(text), stdin);
-    printf("Enter shift value (1-25): ");
-    scanf("%d", &shift);
-    caesarCipher(text, shift);
-    printf("Encrypted string: %s", text);
-    
-    return 0;
+int main(){
+	int k;
+	char text[100];
+	printf("enter the string");
+	fgets(text,sizeof(text),stdin);
+	printf("enter k :");
+	scanf("%d",&k);
+	CC(text,k);
+	printf("encrpted text is:%s\n",text);
+	return 0;
 }
-
